c4a9a3380b93438a7021f58cfd2fecd9581ef958
ysj <sujin3473@gmail.com>
2022-01-25 23:15:50 +0900

    파일 정리

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+  pageName: string;
+}
+function Header(props: Props): React.ReactElement {
+  return (
+  <header className="header">
+    
+  </header>
+  );
+} 
+
+export default Header;
